28af25baaccea3866789118ced309b9ff4e279e9
Jascha Muller <jascha.muller@gmail.com>
2021-10-11 22:37:47 +0200

    postgis manual

--- pi_postgis/init.sql
+++ pi_postgis/init.sql
@@ -1,7 +1,6 @@
 CREATE USER eoed WITH PASSWORD 'userEOEDs2';
 CREATE DATABASE eoed_webapp;
 GRANT ALL PRIVILEGES ON DATABASE eoed_webapp TO eoed;
-ALTER DATABASE eoed_webapp SET 'search_path' = public,shared_extension;
-
-eoed_webapp #= CREATE EXTENSION IF NOT EXISTS POSTGIS;
-eoed_webapp #= CREATE EXTENSION IF NOT EXISTS POSTGIS_TOPOLOGY;
+/*ALTER DATABASE eoed_webapp SET 'search_path' = public,shared_extension;*/
+/*eoed_webapp #= CREATE EXTENSION IF NOT EXISTS POSTGIS;
+eoed_webapp #= CREATE EXTENSION IF NOT EXISTS POSTGIS_TOPOLOGY;*/
